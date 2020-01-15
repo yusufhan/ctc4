6e132f661148ad67218e928d1aaa93e61eae6232
Yusufhan Karakoç <46138164+yusufhan@users.noreply.github.com>
2020-01-15 22:30:26 +0300

    Add files via upload

---
+++ ctc4.php
@@ -0,0 +1,64 @@
+<?php
+
+
+function ctc4($word) {
+	$chars = array(
+	"a" => "yca1",
+	"b" => "sbscy",
+    "c" => "bbt2",
+	"ç" => "ch3e",
+	"d" => "lao4",
+	"e" => "tSdc",
+	"f" => "45aa",
+	"g" => "cn75",
+	"ğ" => "ysUf",
+	"h" => "lxar",
+	"ı" => "prto",
+	"i" => "xmfAN",
+	"j" => "75ard",
+	"k" => "nbmH",
+	"l" => "Ohcz",
+	"m" => "reDF",
+	"n" => "opwEr",
+	"o" => "LVgm",
+	"ö" => "0t5a",
+	"p" => "041s",
+	"r" => "6v5i",
+	"s" => "ist34",
+	"ş" => "tdK1",
+	"t" => "j0exi",
+	"u" => "cmgL",
+	"ü" => "pqewr",
+	"v" => "ccXg",
+	"x" => "43%c",
+	"y" => "0%&v",
+	"z" => "azZe",
+	"w" => "-qew",
+	"q" => "xg1*",
+	"0" => "xoer",
+	"1" => "pxRT",
+	"2" => "VMAd",
+	"3" => "GLHa",
+	"4" => "xRtl",
+	"5" => "hfSm",
+	"6" => "50dgf",
+	"7" => "cTc4",
+	"8" => "krKc",
+	"9" => "bbt2",
+	 0 => "CRLOe",
+	 1 => "bir1",
+	 2 => "i45k",
+	 3 => "xcrl4",
+	 4 => "mc+O5",
+	 5 => "rclg_",
+	 6 => "otic6o",
+	 7 => "ovgm5a",
+	 8 => "ocffhi9",
+	 9 => "xofm3",
+		
+	);
+	$word = strtr(strtolower($word),$chars);
+	return $word;
+
+}
+?>
